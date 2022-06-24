3e25e944c7e58cb8cf0b0c4ca2fec1a7a4b4ff89
Chanmuzi <101971295+Chanmuzi@users.noreply.github.com>
2022-06-24 18:28:38 +0900

    종합

---
+++ 6083.py
@@ -0,0 +1,11 @@
+﻿r,g,b = input().split()
+r = int(r)
+g = int(g)
+b = int(b)
+case = 0
+for x in range(r):
+    for y in range(g):
+        for z in range(b):
+            case += 1
+            print(x,y,z)
+print(case)
