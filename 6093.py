d50f52241314d2490d721c7da69add576a3abe25
Chanmuzi <101971295+Chanmuzi@users.noreply.github.com>
2022-06-25 16:29:20 +0900

    리스트

---
+++ 6093.py
@@ -0,0 +1,11 @@
+﻿n = int(input())
+a = input().split()
+
+d = []  # 공백 리스트 생성
+for i in a:
+    i = int(i)
+    d.append(i) # 입력 받은 값들을 공백 리스트에 추가
+
+d.reverse() # 리스트 뒤집기
+for i in d:
+    print(i,end=" ")
